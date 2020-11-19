6607063a5611d44b2197f9174359921122908b21
AlexPetrik <alex.petrik83@gmail.com>
2020-11-19 22:02:28 +0300

    нажатие на кнопку Назад

--- app/src/main/java/ru/alexpetrik/androidacademyglobalhomework/MovieDetailsActivity.kt
+++ app/src/main/java/ru/alexpetrik/androidacademyglobalhomework/MovieDetailsActivity.kt
@@ -2,10 +2,16 @@ package ru.alexpetrik.androidacademyglobalhomework
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MovieDetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie_details)
+
+        val back_btn = findViewById<Button>(R.id.back_btn)
+        back_btn.setOnClickListener {
+            onBackPressed()
+        }
     }
 }
