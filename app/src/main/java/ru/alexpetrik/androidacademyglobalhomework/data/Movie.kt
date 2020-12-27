f688ae7d1afd8a3638d604fb98c92fb4e2359e44
AlexPetrik <alex.petrik83@gmail.com>
2020-12-28 00:21:30 +0300

    Parcelable с помощью Parcelize (kotlin)

--- app/src/main/java/ru/alexpetrik/androidacademyglobalhomework/data/Movie.kt
+++ app/src/main/java/ru/alexpetrik/androidacademyglobalhomework/data/Movie.kt
@@ -1,9 +1,9 @@
 package ru.alexpetrik.androidacademyglobalhomework.data
 
-import android.os.Parcel
 import android.os.Parcelable
-import java.util.ArrayList
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 data class Movie(
     val id: Int,
     val title: String?,
@@ -16,47 +16,4 @@ data class Movie(
     val runtime: Int,
     val genres: List<Genre>?,
     val actors: List<Actor>?
-) : Parcelable {
-    constructor(parcel: Parcel) : this(
-        parcel.readInt(),
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readFloat(),
-        parcel.readInt(),
-        parcel.readInt(),
-        parcel.readInt(),
-        parcel.createTypedArrayList(Genre),
-        parcel.createTypedArrayList(Actor)
-    ) {
-    }
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeInt(id)
-        parcel.writeString(title)
-        parcel.writeString(overview)
-        parcel.writeString(poster)
-        parcel.writeString(backdrop)
-        parcel.writeFloat(ratings)
-        parcel.writeInt(numberOfRatings)
-        parcel.writeInt(minimumAge)
-        parcel.writeInt(runtime)
-        parcel.writeTypedList(genres)
-        parcel.writeTypedList(actors)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<Movie> {
-        override fun createFromParcel(parcel: Parcel): Movie {
-            return Movie(parcel)
-        }
-
-        override fun newArray(size: Int): Array<Movie?> {
-            return arrayOfNulls(size)
-        }
-    }
-}
+) : Parcelable
